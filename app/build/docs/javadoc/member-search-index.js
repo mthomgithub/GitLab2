869bc041a40f780a76993b0f7f9419b498d6341d
Michael Thompson <mthom040@odu.edu>
2021-11-10 00:35:16 -0500

    Actually deleted Everything

--- app/build/docs/javadoc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"GitLab2","c":"App","l":"App()","url":"%3Cinit%3E()"},{"p":"GitLab2","c":"App","l":"getGreeting()"},{"p":"GitLab2","c":"App","l":"main(String[])","url":"main(java.lang.String[])"}]
